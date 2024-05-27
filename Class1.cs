f9a6b48584fe077b4f6da8f7bf831f59a9f3e789
Ondrul <ondrej.ulovec@gmail.com>
2024-05-27 11:55:12 +0200

    Data model

--- Class1.cs
+++ Class1.cs
@@ -1,24 +1,21 @@
-﻿using System;
-
-public class Class1
+﻿namespace Wheater_data_Analysis_API.Models
 {
-	public WeatherData()
-	{
-
-    public float Precipitation { get; set; }
-    public string DateFull { get; set; } 
-    public int DateMonth { get; set; }
-    public int DateWeekOf {  get; set; }
-    public int DateYear { get; set; }
-    public string StationCity { get; set; }
-    public string StationCode { get; set; }
-    public string StationLocation { get; set; }
-    public string StationState { get; set; }
-    public int DataTemperatureAvgTemp { get; set; }
-    public int DataTemperatureMaxTemp { get; set; }
-    public int DataTemperatureMinTemp { get; set; }
-    public int DataWindDirection { get; set; }
-    public int DataWindSpeed { get; set; }
-
+    public class WeatherData
+    {
+        public float DataPrecipitation { get; set; }
+        public string DateFull { get; set; }
+        public int DateMonth { get; set; }
+        public int DateWeekOf { get; set; }
+        public int DateYear { get; set; }
+        public string StationCity { get; set; }
+        public string StationCode { get; set; }
+        public string StationLocation { get; set; }
+        public string StationState { get; set; }
+        public int DataTemperatureAvgTemp { get; set; }
+        public int DataTemperatureMaxTemp { get; set; }
+        public int DataTemperatureMinTemp { get; set; }
+        public int DataWindDirection { get; set; }
+        public float DataWindSpeed { get; set; }
     }
 }
+
