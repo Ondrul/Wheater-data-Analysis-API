25913e20f96b955cbfc3e137563b0ca8a836a9fa
Ondrul <ondrej.ulovec@gmail.com>
2024-05-27 11:44:36 +0200

    Init project

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Class1
+{
+	public WeatherData()
+	{
+
+    public float Precipitation { get; set; }
+    public string DateFull { get; set; } 
+    public int DateMonth { get; set; }
+    public int DateWeekOf {  get; set; }
+    public int DateYear { get; set; }
+    public string StationCity { get; set; }
+    public string StationCode { get; set; }
+    public string StationLocation { get; set; }
+    public string StationState { get; set; }
+    public int DataTemperatureAvgTemp { get; set; }
+    public int DataTemperatureMaxTemp { get; set; }
+    public int DataTemperatureMinTemp { get; set; }
+    public int DataWindDirection { get; set; }
+    public int DataWindSpeed { get; set; }
+
+    }
+}
